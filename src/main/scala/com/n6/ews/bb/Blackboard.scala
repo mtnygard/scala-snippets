a4114e923eca578816b73472e149100402f6f5dd
Michael Nygard <michael@michaelnygard.com>
2009-08-21 00:20:28 -0500

    Back off of actors while improving typesafety. Temporary lose thread safety.

--- src/main/scala/com/n6/ews/bb/Blackboard.scala
+++ src/main/scala/com/n6/ews/bb/Blackboard.scala
@@ -1,40 +1,20 @@
 package com.n6.ews.bb
 
-
-import scala.actors.Actor
-import scala.actors.Actor._
-
 object Blackboard {
+  import scala.reflect.Manifest
+  import scala.collection.immutable.Map
 
-  def put(value: Any) = board ! Put(value)
-  def take(f: PartialFunction[Any, Boolean]): List[Any] = {
-    board !? Take(f) match {
-      case l: List[Any] => l
-      case _ => List()
-    }
+  private var values = Map.empty[Any, Manifest[_]]
+
+  def put[T](value: T)(implicit m: Manifest[T]) = { 
+    values = values + (value -> m) 
   }
 
-  /* messages to the blackboard */
-  case class Take(template: Any)
-  case class Put(value: Any)
-
-  private val board = new Board start
-
-  private class Board extends Actor {
-    var values: List[Any] = List()
-
-    def act() = loop {
-      receive {
-        case Put(value) =>
-          value match {
-            case list: List[Any] => values = list ::: values
-            case single: Any => values = single :: values
-          }
-
-        case Take(f: PartialFunction[Any, Boolean]) =>
-          val matches = values.filter(f)
-          reply(matches)
-      }
-    }
-  }
+  def take[T](f: (T) => Boolean)(implicit m: Manifest[T]): List[T] =
+    for {
+      k <- values.keys.toList
+      if (values(k) <:< m)
+      value = k.asInstanceOf[T]
+      if f(value)
+    } yield value
 }
--- src/test/scala/com/n6/ews/bb/BlackboardSpecs.scala
+++ src/test/scala/com/n6/ews/bb/BlackboardSpecs.scala
@@ -13,18 +13,13 @@ object BlackboardSpecs extends Specification with JUnit {
       Blackboard put "Firstname Lastname Address"
       Blackboard put 1.0
 
-      val smallIntegers = Blackboard take {
-        case n: Int => n < 1000000
-        case _ => false
-      }
+      val smallIntegers = Blackboard.take[Int] { n => n < 1000000 }
 
-      smallIntegers must haveSize(1)
       smallIntegers must contain(42)
+      smallIntegers must haveSize(1)
 
-      val strings = Blackboard take {
-        case s: String => true
-        case _ => false
-      }
+      val strings = Blackboard.take[String] { _ => true }
+
 
       strings must haveSize(2)
       strings must contain("A string")
@@ -32,14 +27,39 @@ object BlackboardSpecs extends Specification with JUnit {
 
       Blackboard put "Another string"
 
-      val aStrings = Blackboard take {
-        case s: String => s.startsWith("A")
-        case _ => false
-      }
+      val aStrings = Blackboard.take[String] { _.startsWith("A") }
+
 
       aStrings must haveSize(2)
       aStrings must contain("A string")
       aStrings must contain("Another string")
+    }
+  
+    "store and retrieve case classes" in {
+      case class Evidence(items: List[Int])
+      case class Person(firstname: String, lastname: String) {
+        def mathematician = firstname.startsWith("Leo")
+      }
+
+      Blackboard put Evidence(List(1, 1, 3, 5, 8, 13, 21, 35))
+      Blackboard put Person("Leonardo", "Pisano")
+      Blackboard put Person("Leonardo", "da Vinci")
+      Blackboard put Person("Leonhard", "Euler")
+      Blackboard put Person("Wilhelm", "Wien")
+
+      val numbers = Blackboard.take[Evidence] { _ => true }
+
+      numbers must haveSize(1)
+      numbers(0).items must haveSize(8)
+
+      val names = Blackboard.take[Person] { _.mathematician }
+
+
+      names must haveSize(3)
+      
+      names foreach { n =>
+        n must verify ( _.firstname.startsWith("Leon") )
+      }
     }
   }
 }
